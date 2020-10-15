f4e4a4fa2c4a1e6400e16052bdbe97aea620e2b5
laurencun <lauren.busbee@gmail.com>
2020-10-15 12:24:38 -0500

    associations, controllers and views setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :bloggers, only: [:index, :show, :new]
+  resources :posts, only: [:show, :new, :edit]
+  resources :destinations, only: [:show]
+
 end
