b3938e36d893ebb6099d7de8ff95b855eaabc19d
laurencun <lauren.busbee@gmail.com>
2020-10-15 16:09:46 -0500

    working on model methods to utilize in show pages

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -8,4 +8,19 @@ class Blogger < ApplicationRecord
     validates :age, numericality: { greater_than: 0 }
     validates :bio, length: { minimum: 30 }
 
+    def total_likes
+        count = 0
+        total_likes = 0
+        self.posts do |p|
+            while count < self.posts.length
+            total_likes += p.likes
+            end
+        end
+     total_likes
+    end
+
+    def most_liked_post
+        self.posts.where(likes: Post.maximum(:likes))
+    end
+
 end
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -3,4 +3,19 @@ class Destination < ApplicationRecord
     has_many :posts
     has_many :bloggers, through: :posts
 
+
+    def most_recent_posts
+        self.posts.order('created_at DESC').limit(5)
+    end
+
+    def most_liked_post
+        self.posts.where(likes: Post.maximum(:likes))
+    end
+
+    def average_blogger_age
+        ages = self.posts.map {|p| p.blogger.age}
+        unique_ages = ages.uniq
+        unique_ages.sum / self.posts.count
+    end
+
 end
